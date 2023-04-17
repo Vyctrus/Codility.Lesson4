d3bbd9c67fe7b1add0b17c141b1aadbae5dae812
Vyctrus <and.gierlak@gmail.com>
2023-04-17 21:49:45 +0200

    Permutation Check solved using stream+ complexity O(N)

--- src/main/java/org/example/PermCheck.java
+++ src/main/java/org/example/PermCheck.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class PermCheck {
     public int solution(int[] A){
-        return -1;
+        Boolean[] numberOccured=new Boolean[A.length];
+        for(int i=0;i<A.length;i++){
+            if(A[i]-1 <0 || A[i]-1 >= A.length){
+                return 0;
+            }
+            numberOccured[A[i]-1]=true;
+        }
+        return (Arrays.stream(numberOccured).anyMatch(v-> v==false))? 0: 1;
     }
 }
