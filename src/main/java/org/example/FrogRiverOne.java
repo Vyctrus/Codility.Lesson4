527d695f80d8fef8010bdef7313559baa1cfd083
Vyctrus <and.gierlak@gmail.com>
2023-04-17 11:29:00 +0200

    Initial commit

---
+++ src/main/java/org/example/FrogRiverOne.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class FrogRiverOne {
+    public int solution(int X,int[] A){
+
+        int[] leavesFirstTime=new int[X];
+        Arrays.fill(leavesFirstTime, -1);
+        for(int i=0;i<leavesFirstTime.length;i++){
+            if(leavesFirstTime[A[i]-1]==-1){
+                leavesFirstTime[A[i]-1]=i;
+            }
+        }
+        int minTimeNeeded=-1;
+        for(int i=0;i< A.length;i++){
+            if(leavesFirstTime[i]==-1){
+                return -1; //any leaf didnt appear in this place
+            }
+            minTimeNeeded=(leavesFirstTime[i]>minTimeNeeded)? leavesFirstTime[i] : minTimeNeeded;
+        }
+        return minTimeNeeded;
+    }
+
+    //Theory
+    //do kazdego potrzebnego liscia przypisz pierwsze wystapienie
+    //wyznacz max z tablicy do ktorej przypisuje tzn wystapienie ostatniego liscia,
+    //pamietaj o tym ze moga sie nie pojawic wsyzstkie
+}
---
+++ src/test/java/TestFrogRiverOne.java
@@ -0,0 +1,25 @@
+import org.example.FrogRiverOne;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class TestFrogRiverOne {
+
+    static Stream<Arguments> methodSourceArgs() {
+        return Stream.of(
+                arguments(5,new int[]{1, 3,1,4,2,3,5,4}, 6)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("methodSourceArgs")
+    public void solutionTest(int X, int[] A, int expected) {
+        FrogRiverOne s = new FrogRiverOne();
+        assertTrue((s.solution(X,A)==expected));
+    }
+}
