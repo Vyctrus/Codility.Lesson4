118b5688643269b759c69c360e37fe3c235678b6
Vyctrus <and.gierlak@gmail.com>
2023-04-18 10:22:37 +0200

    MaxCounters first concept, solution complexity O(N)

--- src/main/java/org/example/MaxCounters.java
+++ src/main/java/org/example/MaxCounters.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class MaxCounters {
+    public int[] solution(int N,int[] A){
+        int rememberMax=0;
+        int[] result= new int[A.length];
+        Arrays.fill(result,0);
+        for(int i=0;i<A.length;i++){
+            if(A[i]==A.length){
+                A= maxCounter(A, rememberMax);
+            }else{
+                result[A[i]-1]++;   //-1 bcs data in array should be <1,N>
+                rememberMax= (result[A[i]-1] >= rememberMax)? result[A[i]-1]: rememberMax;
+            }
+        }
+        return new int[]{-1};
+    }
+
+
+    private int[] maxCounter(int[] A, int remMax){
+        int[] temp= new int[A.length];
+        for(int i=0;i<A.length;i++){
+            temp[i]= remMax;
+        }
+        return temp;
+    }
 }
--- src/test/java/TestMaxCounters.java
+++ src/test/java/TestMaxCounters.java
@@ -1,2 +1,26 @@
+import org.example.MaxCounters;
+import org.example.PermCheck;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class TestMaxCounters {
+    static Stream<Arguments> methodSourceArgs() {
+        return Stream.of(
+                arguments(5,new int[]{3,4,4,6,1,4,4}, new int[]{3,2,2,4,2})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("methodSourceArgs")
+    public void solutionTest(int N,int[] A, int[] expected) {
+        MaxCounters s = new MaxCounters();
+        assertTrue((s.solution(N,A).equals(expected)));
+    }
+
 }
